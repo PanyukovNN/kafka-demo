691bee141c87237bd911d0c21d569b68620c1aab
Nikolay Panyukov <panyukovnn@gmail.com>
2023-02-04 15:59:52 +0300

    initial commit

---
+++ ms-processor/src/main/java/org/panyukovnn/msprocessor/MsProcessorApplication.java
@@ -0,0 +1,13 @@
+package org.panyukovnn.msprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsProcessorApplication.class, args);
+    }
+
+}
---
+++ ms-sender/src/main/java/org/panyukovnn/mssender/MsSenderApplication.java
@@ -0,0 +1,13 @@
+package org.panyukovnn.mssender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsSenderApplication.class, args);
+    }
+
+}
